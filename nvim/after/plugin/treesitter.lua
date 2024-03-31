d84f90853d667c5029c166d71f068b59eb22ad2d
Dinank <10484407+daynum@users.noreply.github.com>
2024-03-31 09:30:02 +0530

    remove clang from treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -3,8 +3,8 @@ require("nvim-treesitter.configs").setup({
 						"javascript", "html", "markdown", "cpp"},
 	sync_install = false,
 	auto_install = true,
-	-- keep enable as false for windows for now, something makes nvim crash on enabling this
+	-- make enable=false for windows for now, something makes nvim crash on enabling this
 	-- I have tried putting clang as compiler, but didnt work, need more investigation.
-	highlight = { enable = false, additional_vim_regex_highlighting = false },
+	highlight = { enable = true, additional_vim_regex_highlighting = false },
 	indent = { enable = true },
 })
--- nvim/lua/sharpfox/plugins/treesitter.lua
+++ nvim/lua/sharpfox/plugins/treesitter.lua
@@ -2,7 +2,7 @@ local M = {
     "nvim-treesitter/nvim-treesitter",
     build = function()
         require 'nvim-treesitter.install'.prefer_git = false
-        require 'nvim-treesitter.install'.compilers = { "clang" }
+        -- require 'nvim-treesitter.install'.compilers = { "clang" }
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
 }
