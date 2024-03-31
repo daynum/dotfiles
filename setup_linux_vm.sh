12ee8a90473f5d7fb619e3f7bd2a760af8a3ad3f
Dinank <10484407+daynum@users.noreply.github.com>
2024-03-31 09:17:38 +0530

    script to install initial setup packages
    Installs all initial packages needed to setup linux ARCH installation. Cnds are specific for pacman package manager.

---
+++ setup_linux_vm.sh
@@ -0,0 +1,21 @@
+# Update pacman packages
+pacman -Syu
+
+# Install dev tools i use
+pacman -S git gcc make base-devel pyenv
+
+# Install the vmware guest libraries for good compatibility of video etc. (that window resize thing)
+pacman -S open-vm-tools xf86-input-vmmouse xf86-video-vmware mesa gtk2 gtkmm
+
+# Enable the above installed service
+systemctl enable vmtoolsd.service
+systemctl start vmtoolsd.service
+
+
+
+# Install fonts
+pacman -S ttf-hack-nerd noto-fonts noto-fonts-emoji noto-fonts-emoji
+pacman -S firefox neovim
+
+# Install zshell, but we'll set it up at end
+pacman -S zsh
